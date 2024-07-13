795a7f51b2df5d7200159629471f4f1695b12f61
ravi <patryk1987772@wp.pl>
2024-07-13 12:27:59 +0200

    more progress on header

--- Airbnb/wwwroot/js/site.js
+++ Airbnb/wwwroot/js/site.js
@@ -2,15 +2,34 @@
 //  header scaling
 window.onscroll = function() { headerScaling() };
 
+
 function headerScaling() 
 {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1)
     {
-        document.getElementById("header").style.height = "100px";
+        document.getElementById("StaysMenuFormStays").style.top = "25px";
+        document.getElementById("StaysMenuFormExperiences").style.top = "25px";
+        document.getElementById("StaysMenuButtons").style.top = "-50px";
+        document.getElementById("TopHeader").style.height = "60px";
     }
     else 
     {
-        document.getElementById("header").style.height = "200px";
+        document.getElementById("StaysMenuFormStays").style.top = "100px";
+        document.getElementById("StaysMenuFormExperiences").style.top = "100px";
+        document.getElementById("StaysMenuButtons").style.top = "20px";
+        document.getElementById("TopHeader").style.height = "150px";
     }
+}
+
+function displayStaysMenu()
+{
+    document.getElementById("StaysMenuFormStays").style.display = "block";
+    document.getElementById("StaysMenuFormExperiences").style.display = "none";
+}
+
+function displayExperiencesMenu()
+{
+    document.getElementById("StaysMenuFormExperiences").style.display = "block";
+    document.getElementById("StaysMenuFormStays").style.display = "none";
 }
 
