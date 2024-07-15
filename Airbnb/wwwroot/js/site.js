95ffb67a8847b1e433a178cf183606f12d3cc3f4
ravi <patryk1987772@wp.pl>
2024-07-15 13:08:03 +0200

    moved scroll bar to main page instead of being in header, improved scrolling in it and other small improvements

--- Airbnb/wwwroot/js/site.js
+++ Airbnb/wwwroot/js/site.js
@@ -2,21 +2,34 @@
 //  header scaling
 window.onscroll = function() { headerScaling() };
 
+
 function headerScaling() 
 {
     if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1)
     {
-        document.getElementById("StaysMenuFormStays").style.top = "25px";
-        document.getElementById("StaysMenuFormExperiences").style.top = "25px";
+        //  if scrolled down
+        document.getElementById("TopHeader").style.height = "60px";
+
+        document.getElementById("StaysMenuFormStays").style.top = "35px";
+        document.getElementById("StaysMenuFormExperiences").style.top = "35px";
+
         document.getElementById("StaysMenuButtons").style.top = "-50px";
-        document.getElementById("TopHeader").style.paddingBottom = "10px";
+
+        document.getElementById("ScrollDiv").style.top = "69px";
+        document.getElementById("ScrollDiv").style.boxShadow = "0 4px 6px -6px #cacaca";
     }
     else 
     {
+        //  if on top of page
+        document.getElementById("TopHeader").style.height = "150px";
+
         document.getElementById("StaysMenuFormStays").style.top = "100px";
         document.getElementById("StaysMenuFormExperiences").style.top = "100px";
+
         document.getElementById("StaysMenuButtons").style.top = "20px";
-        document.getElementById("TopHeader").style.paddingBottom = "80px";
+
+        document.getElementById("ScrollDiv").style.top = "150px";
+        document.getElementById("ScrollDiv").style.boxShadow = "none";
     }
 }
 
@@ -34,16 +47,36 @@ function displayExperiencesMenu()
 
 
 //  scroll menu
-var buttonLeft = document.getElementById("ScrollLeft");
-var buttonRight = document.getElementById("ScrollRight");
+const buttonLeft = document.getElementById("ScrollLeft");
+const buttonRight = document.getElementById("ScrollRight");
+const scrollMenu = document.getElementById("ScrollMenu");
 
+buttonLeft.style.display = "none"; 
+scrollMenu.scrollLeft == 0;
+
+/* left button */
 buttonLeft.onclick = function()
 {
-    document.getElementById("ScrollMenu").scrollLeft -= 650;
+    scrollMenu.scrollLeft -= 650;
+
+    buttonRight.style.display = "block";  
+    
+    if (scrollMenu.scrollLeft <= 650)
+    {
+        buttonLeft.style.display = "none";
+    }
 }
 
+/* right button */
 buttonRight.onclick = function()
 {
-    document.getElementById("ScrollMenu").scrollLeft += 650;
+    scrollMenu.scrollLeft += 650;
+
+    buttonLeft.style.display = "block";
+
+    if (scrollMenu.scrollLeft >= 4300 )
+    {
+        buttonRight.style.display = "none";
+    }
 }
 
