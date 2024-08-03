0faa8fd76e46c14b0589a9c0e75e71285677a900
ravi <patryk1987772@wp.pl>
2024-08-03 14:02:16 +0200

    finally can close dropdown windows when clicking outside of their area, all dropdown windows have correct size and are in correct places and more or less done who form button dropdown window

--- Airbnb/wwwroot/js/site.js
+++ Airbnb/wwwroot/js/site.js
@@ -55,6 +55,12 @@ function headerScaling()
     document.getElementById("GreyBackground").style.display = "none";
 
     whereDropdown.style.display = "none";
+    checkinDropdown.style.display = "none";
+    checkoutDropdown.style.display = "none";
+    whoDropdown.style.display = "none";
+    whereDropdownExperiences.style.display = "none";
+    datesDropdown.style.display = "none";
+    whoDropdownExperiences.style.display = "none";
 }
 
 /* ------------------------------- BUTTONS ------------------------------- */
@@ -131,8 +137,7 @@ currenciesButton.onclick = function()
     languages.style.display = "none";
 }
 
-// box on click but scuffed and will fix soonTM
-
+// FORM BUTTONS ITS SCUFFED BUT ITS MINE AND I WILL PROTECT THIS SHIT CODE WITH MY ROTTEN FISH SWORD
 const whereButton = document.getElementById("WhereButton");
 const whereDropdown = document.getElementById("WhereDropdown");
 
@@ -154,76 +159,116 @@ const datesDropdown =  document.getElementById("DatesDropdown");
 const whoButtonExperiences = document.getElementById("WhoButtonExperiences");
 const whoDropdownExperiences = document.getElementById("WhoDropdownExperiences");
 
-var banana;
-
-
-if (banana == true)
+function onClickOutside(dropdown, button, cb) 
 {
-    document.body.addEventListener("click", function() 
+    document.addEventListener('click', event => 
     {
-        console.log("ASDOYHBUA")
-        whereDropdown.style.display = "none";
-        banana = false;
-    })
-}
+        if (!dropdown.contains(event.target) && !button.contains(event.target)) 
+        {
+            if (dropdown.style.display == "block")
+            {
+                cb();
+            }
+        }
+    });
+};
 
-whereButton.onclick = function()
+onClickOutside(whereDropdown, whereButton, function()
 {
+    whereDropdown.style.display = "none";
+})
+
+onClickOutside(checkinDropdown, checkinButton, function()
+{
+    checkinDropdown.style.display = "none";
+})
+
+onClickOutside(checkoutDropdown, checkoutButton, function()
+{
+    checkoutDropdown.style.display = "none";
+})
+
+onClickOutside(whoDropdown, whoButton, function()
+{
+    whoDropdown.style.display = "none";
+})
+
+onClickOutside(whereDropdownExperiences, whereButtonExperiences, function()
+{
+    whereDropdownExperiences.style.display = "none";
+})
+
+onClickOutside(datesDropdown, datesButton, function()
+{
+    datesDropdown.style.display = "none";
+})
+
+onClickOutside(whoDropdownExperiences, whoButtonExperiences, function()
+{
+    whoDropdownExperiences.style.display = "none";
+})
+
+whereButton.onclick = function(e)
+{
+    e.preventDefault();
     whereDropdown.style.display = "block";
 
-   
-
-    banana = true;
-    //checkinDropdown.style.display = "none";
-    //checkoutDropdown.style.display = "none";
-    //whoDropdown.style.display = "none";
+    checkinDropdown.style.display = "none";
+    checkoutDropdown.style.display = "none";
+    whoDropdown.style.display = "none";
 }
 
-checkinButton.onclick = function()
+checkinButton.onclick = function(e)
 {
+    e.preventDefault();
     checkinDropdown.style.display = "block";
 
-    //whereDropdown.style.display = "none";
-    //checkoutDropdown.style.display = "none";
-    //whoDropdown.style.display = "none";
+    whereDropdown.style.display = "none";
+    checkoutDropdown.style.display = "none";
+    whoDropdown.style.display = "none";
 }
 
-checkoutButton.onclick = function()
+checkoutButton.onclick = function(e)
 {
+    e.preventDefault();
     checkoutDropdown.style.display = "block";
 
-    //whereDropdown.style.display = "none";
-    //checkinDropdown.style.display = "none";
-    //whoDropdown.style.display = "none";
+    whereDropdown.style.display = "none";
+    checkinDropdown.style.display = "none";
+    whoDropdown.style.display = "none";
 }
 
-whoButton.onclick = function()
+whoButton.onclick = function(e)
 {
+    e.preventDefault();
     whoDropdown.style.display = "block";
 
-    //whereDropdown.style.display = "none";
-    //checkinDropdown.style.display = "none";
-    //checkoutDropdown.style.display = "none";
+    whereDropdown.style.display = "none";
+    checkinDropdown.style.display = "none";
+    checkoutDropdown.style.display = "none";
 }
 
-whereButtonExperiences.onclick = function()
+whereButtonExperiences.onclick = function(e)
 {
+    e.preventDefault();
     whereDropdownExperiences.style.display = "block";
 
     datesDropdown.style.display = "none";
     whoDropdownExperiences.style.display = "none";
 }
 
-datesButton.onclick = function()
+datesButton.onclick = function(e)
 {
+    e.preventDefault();
     datesDropdown.style.display = "block";
 
     whereDropdownExperiences.style.display = "none";
     whoDropdownExperiences.style.display = "none";
 }
 
-whoButtonExperiences.onclick = function()
+whoButtonExperiences.onclick = function(e)
 {
+    e.preventDefault();
     whoDropdownExperiences.style.display = "block";
 
     whereDropdownExperiences.style.display = "none";
@@ -291,6 +336,65 @@ addGuestsButton.onclick = function()
      document.getElementById("StaysMenuForm").style.display = "block";
 
      document.getElementById("GreyBackground").style.display = "block";
-     console.log("capybara")
+     console.log("CA PY BA RA capybara capybara capybara capybara")
 }
+
+// FORM DROPDOWN THINGY BUTTONS
+const adultsCount = document.getElementById("AdultsCount");
+const decrementAdultCountButton = document.getElementById("DecreaseAdultCount");
+const incrementAdultCountButton = document.getElementById("IncreaseAdultCount");
+
+const childrenCount = document.getElementById("ChildrenCount");
+const decrementChildCountButton = document.getElementById("DecreaseChildCount");
+const incrementChildCountButton = document.getElementById("IncreaseChildCount");
+
+const infantsCount = document.getElementById("InfantsCount");
+const decrementInfantCountButton = document.getElementById("DecreaseInfantCount");
+const incrementInfantCountButton = document.getElementById("IncreaseInfantCount");
+
+const petsCount = document.getElementById("PetsCount");
+const decrementPetCountButton = document.getElementById("DecreasePetCount");
+const incrementPetCountButton = document.getElementById("IncreasePetCount");
+
+function changeNumber(entity, increment, decrement)
+{
+    increment.onclick = function()
+    {
+        if (increment.style.cursor == "pointer")
+        {
+            entity.innerText ++;
+
+            decrement.style.cursor = "pointer"; 
+
+            if (entity.innerText == 16)
+            {
+                increment.style.cursor = "not-allowed";
+            }
+        }
+    }
+
+    decrement.onclick = function()
+    {
+        if (decrement.style.cursor == "pointer")
+        {
+            entity.innerText --;
+
+            increment.style.cursor = "pointer";
+
+            if (entity.innerText == 0)
+            {
+                decrement.style.cursor = "not-allowed";
+            }
+        }     
+    } 
+}
+
+changeNumber(adultsCount, incrementAdultCountButton, decrementAdultCountButton);
+changeNumber(childrenCount, incrementChildCountButton, decrementChildCountButton);
+changeNumber(infantsCount, incrementInfantCountButton, decrementInfantCountButton);
+changeNumber(petsCount, incrementPetCountButton, decrementPetCountButton);
+
+
+
+
 
